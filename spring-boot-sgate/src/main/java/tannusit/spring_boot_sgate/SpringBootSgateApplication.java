261ca1dd6df2696be8a64013fe715e18c47d0fa3
hong seok Park <hspark@tannus.com>
2025-06-24 15:42:01 +0900

    Feat : first

---
+++ spring-boot-sgate/src/main/java/tannusit/spring_boot_sgate/SpringBootSgateApplication.java
@@ -0,0 +1,13 @@
+package tannusit.spring_boot_sgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSgateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSgateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tannusit/spring_boot_sgate/SpringBootSgateApplicationTests.java
@@ -0,0 +1,13 @@
+package tannusit.spring_boot_sgate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSgateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
